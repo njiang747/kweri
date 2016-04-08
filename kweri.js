0357b2232a67693db2262d78f269a72f9b08f8bb
jonzha <jonzha39@gmail.com>
2016-04-07 23:23:36 -0400

    Cas login 1.0
    working cas login

--- kweri.js
+++ kweri.js
@@ -18,9 +18,12 @@ if (Meteor.isClient) {
     // and decreasing creation date
     questions: function() {
       return Questions.find({}, {sort: {value: -1, createdAt: -1}});
+    },
+    userName: function() {
+      return Meteor.user().profile.name;
     }
-  });
 
+  });
   // Define action to be triggered on certain events within body
   Template.body.events({
     // Action upon submitting a new question
@@ -43,6 +46,20 @@ if (Meteor.isClient) {
       
       // return false (don't reload the page)
       return false;
+    }, 
+    'click .btn': function(){
+      if(Meteor.user()){
+        Meteor.logout();
+        var settings = Meteor.settings.public.cas;
+        openCenteredPopup(
+          "https://fed.princeton.edu/cas/logout",
+          settings.width || 800,
+          settings.height || 600);
+       } else {
+          Meteor.loginWithCas(redirect);
+       }
+     
+      return false;
     }
   });
 
@@ -61,6 +78,7 @@ if (Meteor.isClient) {
     }
   });
 
+
   Accounts.ui.config({
     passwordSignupFields: "USERNAME_ONLY"
   });
@@ -71,3 +89,36 @@ if (Meteor.isServer) {
     // code to run on server at startup
   });
 }
+
+function redirect(error){
+  if(!error){
+    //window.location.replace("http://google.com");
+  } else {
+    alert("Please log in.");
+  }
+}
+var openCenteredPopup = function(url, width, height) {
+  var screenX = typeof window.screenX !== 'undefined'
+  ? window.screenX : window.screenLeft;
+  var screenY = typeof window.screenY !== 'undefined'
+  ? window.screenY : window.screenTop;
+  var outerWidth = typeof window.outerWidth !== 'undefined'
+  ? window.outerWidth : document.body.clientWidth;
+  var outerHeight = typeof window.outerHeight !== 'undefined'
+  ? window.outerHeight : (document.body.clientHeight - 22);
+  // XXX what is the 22?
+
+  // Use `outerWidth - width` and `outerHeight - height` for help in
+  // positioning the popup centered relative to the current window
+  var left = screenX + (outerWidth - width) / 2;
+  var top = screenY + (outerHeight - height) / 2;
+  var features = ('width=' + width + ',height=' + height +
+      ',left=' + left + ',top=' + top + ',scrollbars=yes');
+
+  var newwindow = window.open(url, '_blank', features);
+  if (newwindow.focus)
+    newwindow.focus();
+return newwindow;
+};
+
+//Meteor.user().profile.name
