a6ec1148f574b61c0624c6820f140341515d9f49
jonzha <jonzha39@gmail.com>
2016-04-26 17:31:23 -0400

    Merge branch 'master' into Jon-Branch
    # Conflicts:
    #	client/client.js

--- client/client.js
+++ client/client.js
@@ -17,7 +17,7 @@ Template.main.events({
     Meteor.loginWithCas(function(err){if(err)alert("Failed to login")});
     return false;
   },
-  'click .title-profile-arrow': function(event) {
+  'click .menu-logout': function(event) {
     if(Meteor.user()){
       Meteor.logout();
       openCenteredPopup(
@@ -39,7 +39,11 @@ Template.home.events({
       Meteor.loginWithCas(
         function(err){
           if(err)alert("Failed to login");
-          else Router.go('profileProf');
+          else {
+            Meteor.users.update(Meteor.userId(), 
+              {$set: {"profile.profStatus": 1}});
+            Router.go('profileProf');
+          }
       });
     }
     return false;
@@ -51,18 +55,37 @@ Template.home.events({
       Meteor.loginWithCas(
         function(err){
           if(err)alert("Failed to login");
-          else Router.go('profileStud');
+          else {
+            Meteor.users.update(Meteor.userId(), 
+              {$set: {"profile.profStatus": 0}});
+            Router.go('profileStud');
+          }
       });
     }
     return false;
+  },
+  'click .menu-logout': function(event) {
+    if(Meteor.user()){
+      Meteor.logout();
+      openCenteredPopup(
+        "https://fed.princeton.edu/cas/logout",
+        810 || 800,
+        610 || 600);
+      Router.go('home');
+    } 
+    return false;
   }
+
 });
 
 /***** Profile Page ***********************************************************/
 Template.classlist.helpers({
   /* classes returns a list of classes */
   classes: function() {
-    return Classes.find({}, {sort: {listing: 1}});
+    if (Router.current().route.getName() == "profileProf") 
+      return Classes.find({}, {sort: {department: 1, number: 1}});
+    else if (Router.current().route.getName() == "profileStud") 
+      return Classes.find({students: Meteor.userId()}, {sort: {department: 1, number: 1}})
   }
 });
 
@@ -77,7 +100,7 @@ Template.addClass.events({
   /* insert a new class into the Classes collection */
   'submit .new-class': function(event) {
     var department = event.target.department.value.toUpperCase();
-    var number = parseInt(event.target.number.value);
+    var number = event.target.number.value;
     var name = event.target.name.value;
     Classes.insert({
       department: department,
@@ -90,6 +113,55 @@ Template.addClass.events({
     event.target.number.value = "";
     event.target.name.value = "";
     return false
+  }
+});
+
+Template.classSearch.events({
+  'keyup .searchTerm': function(event) {
+    Session.set('searchKey', event.target.value);
+  }
+});
+
+Template.searchClasslist.helpers({
+  /* classes returns a list of classes that match search term(s) */
+  classes: function() {
+    var key = Session.get('searchKey');
+    if (key == null || key == "") return;
+    var name = new RegExp(key, 'i');
+    var arr = key.split(" ").filter(function(n) {return n != ''});
+    var dept = new RegExp();
+    var num = new RegExp();
+    if (arr.length <= 2) {
+      for (index in arr) {
+        if (isNaN(arr[index])) {
+          dept = new RegExp(arr[index], 'i');
+        } else {
+          num = new RegExp(arr[index]);
+        }
+      }
+    } else return;
+    return Classes.find(
+      {$and: [
+        {$or: [
+          {$and: [{department: dept}, {number: num}]}, 
+          {name: name}
+        ]},
+        {students: {$ne: Meteor.userId()}}
+      ]}, 
+      {sort: {department: 1, number: 1}});
+  }
+});
+
+Template.searchClassElem.helpers({
+  prof_names: function() {
+    var prof_ids = this.profs;
+    return Meteor.users.find({_id: prof_ids[0]});
+  }
+});
+
+Template.searchClassElem.events({
+  'click .enroll': function() {
+    Classes.update({_id: this._id}, {$push: {students: Meteor.userId()}})
   }
 });
 
@@ -119,7 +191,8 @@ Template.lecturelist.helpers({
 Template.lecturelist.events({
   /* clicking on a lecture redirects to that lecture's page */
   'click .lecture-listing': function() {
-    Router.go('lecture', {class_id: Router.current().params.class_id, lecture_id: this._id});
+    Router.go('lecture', 
+      {class_id: Router.current().params.class_id, lecture_id: this._id});
   }
 });
 
@@ -133,7 +206,7 @@ Template.addLecture.events({
       number: number,
       name: name,
       confuseList: [],
-      totalList: [Meteor.userId()],
+      totalList: [],
       date: new Date(),
       openStatus: true
 
@@ -198,15 +271,13 @@ Template.questionbox.events({
     return false;
   },
   'click .questions-con-button': function(){
-
     var lecture =  Lectures.findOne(Router.current().params.lecture_id);
-    console.log(lecture);
     if (lecture.confuseList.indexOf(Meteor.userId()) == -1) {
       Lectures.update(Router.current().params.lecture_id, 
         {
           $push: {confuseList: Meteor.userId()}
         });
-      setTimeout(confuseTimer, 3000);
+      var confuseTimerReset = setTimeout(confuseTimer(lecture), 60000);
     } else {
       Lectures.update(Router.current().params.lecture_id, 
         {
@@ -216,7 +287,10 @@ Template.questionbox.events({
     return false;
   }
 });
-var confuseTimer = function() {
+var confuseTimer = function(lecture) {
+    if (lecture.confuseList.indexOf(Meteor.userId()) == -1) {
+      return;
+    }
       alert("1 minute elapsed, confusion status cleared");
       Lectures.update(Router.current().params.lecture_id, 
         {
@@ -276,7 +350,8 @@ Template.questionConCounter.helpers({
   },
   color: function() {
     var lecture =  Lectures.findOne(Router.current().params.lecture_id);
-    var per = Math.floor(lecture.confuseList.length/lecture.totalList.length*100);
+    var per = 
+      Math.floor(lecture.confuseList.length/lecture.totalList.length*100);
     if (per <= 25){
       return "progress-bar-success";
     } else if (per <= 50){
@@ -288,12 +363,10 @@ Template.questionConCounter.helpers({
 });
 
 Template.questionConCounter.events({
+    /* Reset cc counter */
     'click .questions-conReset-button': function(){
-    console.log("TEST");
-
       var lecture =  Lectures.findOne(Router.current().params.lecture_id);
       Lectures.update(Router.current().params.lecture_id, { $set : {confuseList: [] }} , {multi:true} );
-
     return false;
   }
 
